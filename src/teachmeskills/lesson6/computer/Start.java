5004fde99ba70754d9524217d6fa615477edb04a
RomanSavitskiy <roma.savitskiy@inbox.ru>
2022-07-26 19:53:42 +0300

    Cleaned the code

--- src/teachmeskills/lesson6/computer/Start.java
+++ src/teachmeskills/lesson6/computer/Start.java
@@ -9,7 +9,7 @@ public class Start {
         int computerPrice1 = 100;
         String computerModel = "HP";
 
-        Computer computer1 = new Computer(computerPrice1, computerModel);
+        Computer firstComputer = new Computer(computerPrice1, computerModel);
 
         String nameHDD = "Seagate";
         long sizeHDD = 2048L;
@@ -25,9 +25,9 @@ public class Start {
         int computerPrice2 = 200;
         String computerModel2 = "Asus";
 
-        Computer computer2 = new Computer(computerPrice2, computerModel2, RAM1, HDD1 );
+        Computer secondComputer = new Computer(computerPrice2, computerModel2, RAM1, HDD1 );
 
-        System.out.println(computer1);
-        System.out.println(computer2);
+        System.out.println(firstComputer);
+        System.out.println(secondComputer);
     }
 }
