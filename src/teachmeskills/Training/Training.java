9eb47b987ef8ce219072c3ada82596b140fd31c0
RomanSavitskiy <roma.savitskiy@inbox.ru>
2022-08-26 19:31:43 +0300

    Added lesson 12 homework

--- src/teachmeskills/Training/Training.java
+++ src/teachmeskills/Training/Training.java
@@ -1,23 +1,36 @@
 package teachmeskills.Training;
 
+import teachmeskills.lesson12.service.FileService;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class Training {
-  public static void main(String[] args)
-  {
-    int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
-    int temp;
+  public static void main(String[] args) throws IOException {
+   /* BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("documentNumber.txt")));
 
-    for (int i = 0; i < array.length; i++) {
-      for (int j = 0; j < array.length - 1; j++) {
-        if (array[j] > array[j + 1]) {
-          temp = array[j];
-          array[j] = array[j + 1];
-          array[j + 1] = temp;
-        }
-      }
+    bufferedWriter.write("dsada");
+
+    bufferedWriter.close();*/
+
+    FileWriter fileWriter = new FileWriter("C:\\TeachMeSkill\\TeachMeSkillsRepo\\src\\teachmeskills\\Training\\documentNumber1.txt");
+
+    fileWriter.write("sdadsa");
+    fileWriter.write('\n');
+    fileWriter.write("Helllo world");
+    fileWriter.close();
+
+    FileReader fileReader = new FileReader("C:\\TeachMeSkill\\TeachMeSkillsRepo\\src\\teachmeskills\\Training\\documentNumber1.txt");
+
+    char [] a = new char[100];
+
+    fileReader.read(a);
+
+    for (char symbol : a) {
+      System.out.print(symbol);
     }
 
-    System.out.println(Arrays.toString(array));
   }
 }
